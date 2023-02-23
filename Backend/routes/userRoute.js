ae97187ea1a704d2681b1e1a2838790d3e0501a6
iknow6aint <saintagbukor@gmail.com>
2023-02-23 21:33:29 +0100

    isAdimin middleware

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { getallUser, getaUser, deleteaUser, updatedUser } = require('../controllers/userController');
-const { authMiddleware } = require('../middlewares/authMiddlewears');
+const { authMiddleware, isAdmin } = require('../middlewares/authMiddlewears');
 const userRoute = express.Router();
 
 userRoute.get('/all-users', getallUser)
-userRoute.get("/:id", authMiddleware, getaUser);
+userRoute.get("/:id", authMiddleware, isAdmin, getaUser);
 userRoute.get("/:id", deleteaUser);
 userRoute.get("/:id", updatedUser);
 
