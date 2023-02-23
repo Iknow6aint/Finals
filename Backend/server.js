dd2024e9b79aec94ebbf2a9865e2b9d8c0247a14
iknow6aint <saintagbukor@gmail.com>
2023-02-23 13:57:37 +0100

    create user

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,30 @@
+const { urlencoded } = require('body-parser');
+const bodyParser = require('body-parser');
 const express = require('express');
 const dbConnect = require('./config/dbConnet');
+const authRouter = require('./routes/authRoute');
+const { notFound, errorHandler } = require('./middlewares/errorHandler')
+
+
 const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
 
+//db init
 dbConnect();
 
+
+app.use(bodyParser.json(urlencoded({ extended: false })))
+// Routes
+app.use('/api/user', authRouter)
 app.use('/', (req, res) => {
     console.log('hello world');
 })
+
+app.use(errorHandler)
+app.use(notFound)
+//Listen
 app.listen(PORT, () => {
     console.log(`running on ${PORT}`);
 })
