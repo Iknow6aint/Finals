5b365122d1d24417dae05a15ae3ccf6c456b6bb3
iknow6aint <saintagbukor@gmail.com>
2023-02-26 20:27:15 +0100

    isadmin auth

--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -1,15 +1,15 @@
 const express = require('express')
 
 const { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct } = require("../controllers/productController");
-const { isAdmin } = require('../middlewares/authMiddlewears');
+const { isAdmin, authMiddleware } = require('../middlewares/authMiddlewears');
 
 const productRouter = express.Router();
 
 
-productRouter.post('/create-product', createProduct)
-productRouter.get('/:id', getaProduct)
+productRouter.post('/create-product', authMiddleware, isAdmin, createProduct)
+productRouter.get('/:id', isAdmin, getaProduct)
 productRouter.get('/', getAllProduct);
-productRouter.put('/update/:id', updateProduct);
-productRouter.delete('/delete/:id', deleteProduct);
+productRouter.put('/update/:id', authMiddleware, isAdmin, updateProduct);
+productRouter.delete('/delete/:id', authMiddleware, isAdmin, deleteProduct);
 
 module.exports = productRouter
